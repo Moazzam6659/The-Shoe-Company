580fdd4ce57c333a58e52a30884168cecdd90880
Moazzam <moazzam.ali6659@gmail.com>
2024-09-14 23:24:18 +0530

    Complete all sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       screens: {
         'media560': '560px'
+      },
+      backgroundImage: {
+        'testimonial-section': "url('images/testimonial-section-background.png')"
       }
     },
   },
